8edeb28a9c5adbb3d6594b7a21a0bdb84a997d4d
B3kay <benjaminjekarlsson@gmail.com>
2023-09-22 15:16:50 +0200

    First Commit - Basal Website

---
+++ src/routes/+page.svelte
@@ -0,0 +1,127 @@
+<div class="hero min-h-screen">
+  <div class="hero-content block lg:flex gap-8">
+    <div class="max-w-lg text-center md:text-left">
+      <!-- <h6 class="uppercase">Welcome to Benjamin Karlsson</h6> -->
+      <h1 class="text-5xl font-black contrast-150 brightness-150">
+        Remote first Fullstack Development
+      </h1>
+      <p class="py-9">
+        5 years of experience in front-end development, specializing in
+        TypeScript, React.js, and advanced patterns like hooks, Redux, and
+        routing. Dedicated to creating user-friendly, feature-rich websites.
+        Proficient in Mono repo tools, Styled components, Jest, Storybook, and
+        Git.
+      </p>
+      <div class="join-vertical join md:join-horizontal gap-2">
+        <button class="btn md:btn-lg btn-neutral btn-wide normal-case"
+          >See cover letter</button
+        >
+        <button class="btn md:btn-lg btn-primary btn-wide normal-case"
+          >Get in touch</button
+        >
+      </div>
+    </div>
+    <div class="h-24 lg:hidden" />
+    <div class="mockup-window border border-base-300">
+      <div
+        class="flex justify-center px-4 py-8 md:py-16 border-t border-base-300"
+      >
+        <div>
+          <div class="chat chat-start">
+            <div class="chat-image avatar">
+              <div class="w-10 rounded-full">
+                <img
+                  src="https://images.unsplash.com/photo-1535207010348-71e47296838a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODR8fGZhY2UlMjB3b21hbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+                />
+              </div>
+            </div>
+            <div class="chat-header">
+              You
+              <time class="text-xs opacity-50">5 min ago</time>
+            </div>
+            <div class="chat-bubble">👋Hi there! Impressed by your skills!</div>
+            <!-- <div class="chat-footer opacity-50">Delivered</div> -->
+          </div>
+          <div class="chat chat-end">
+            <div class="chat-image avatar">
+              <div class="w-10 rounded-full">
+                <img
+                  src="https://media.licdn.com/dms/image/C4D03AQFLOxGUx-4V3A/profile-displayphoto-shrink_800_800/0/1663234716531?e=1700697600&v=beta&t=x2Z7F1h_0UAu0NqOPojIHHOO6R-xYGvcBLTcxCDuU7c"
+                />
+              </div>
+            </div>
+            <div class="chat-header">
+              Benjamin K.
+              <time class="text-xs opacity-50">3 min ago</time>
+            </div>
+
+            <div class="chat-bubble">😁Thanks! What's up?</div>
+            <!-- <div class="chat-footer opacity-50">Seen at 12:46</div> -->
+          </div>
+
+          <div class="chat chat-start">
+            <div class="chat-image avatar">
+              <div class="w-10 rounded-full">
+                <img
+                  src="https://images.unsplash.com/photo-1535207010348-71e47296838a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODR8fGZhY2UlMjB3b21hbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+                />
+              </div>
+            </div>
+            <div class="chat-header">
+              You
+              <time class="text-xs opacity-50">1 min ago</time>
+            </div>
+            <div class="chat-bubble">
+              📞 Interested in a chat? Drop your email. 😁😉
+            </div>
+            <!-- <div class="chat-footer opacity-50">Delivered</div> -->
+          </div>
+          <div class="chat chat-end">
+            <div class="chat-image avatar">
+              <div class="w-10 rounded-full">
+                <!-- <img
+                  src="https://media.licdn.com/dms/image/C4D03AQFLOxGUx-4V3A/profile-displayphoto-shrink_800_800/0/1663234716531?e=1700697600&v=beta&t=x2Z7F1h_0UAu0NqOPojIHHOO6R-xYGvcBLTcxCDuU7c"
+                /> -->
+              </div>
+            </div>
+            <div class="chat-header">
+              Benjamin K.
+              <time class="text-xs opacity-50">Just now</time>
+            </div>
+            <div class="chat-bubble">
+              Sure 👍, here it is
+
+              <a
+                class="link link-primary"
+                href="hi@benjaminkarlsson.com"
+                target="_blank"
+                rel="noopener noreferrer">hi@benjaminkarlsson.com</a
+              >
+              or reach me at
+              <a
+                class="link link-primary"
+                href="https://www.linkedin.com/in/benjik/"
+                target="_blank"
+                rel="noopener noreferrer">linkedin.com/in/benjik</a
+              >
+            </div>
+          </div>
+          <div class="chat chat-end">
+            <div class="chat-image avatar">
+              <div class="w-10 rounded-full">
+                <img
+                  src="https://media.licdn.com/dms/image/C4D03AQFLOxGUx-4V3A/profile-displayphoto-shrink_800_800/0/1663234716531?e=1700697600&v=beta&t=x2Z7F1h_0UAu0NqOPojIHHOO6R-xYGvcBLTcxCDuU7c"
+                />
+              </div>
+            </div>
+            <div class="chat-header">
+              <!-- Benjamin K.
+              <time class="text-xs opacity-50">Just now</time> -->
+            </div>
+            <div class="chat-bubble">Have a good time! 😉😁</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
