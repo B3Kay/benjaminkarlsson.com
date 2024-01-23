ec1990d86326c9138f886f9b83e0bd18eba1b6c3
B3Kay <benjaminjekarlsson@gmail.com>
2024-01-23 10:13:14 +0100

    :wastebasket: Clean commented out code & add updated CV

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,12 +26,11 @@
 <div class="hero min-h-screen">
   <div class="hero-content block lg:flex gap-8 p-0">
     <div class="mt-12 sm:mt-0 max-w-lg text-center md:text-left">
-      <!-- <h6 class="uppercase">Welcome to Benjamin Karlsson</h6> -->
       <div class=" badge badge-accent badge-outline mb-4">Remote First</div>
 
       <h1 class="lg:text-7xl md:text-6xl text-4xl font-black">
         <span class="brightness-150 contrast-150">
-          Freelancing Fullstack Excellence
+          Freelancing Frontend Excellence
         </span>
         <img
           class="w-16 inline"
@@ -40,10 +39,11 @@
         />
       </h1>
       <p class="py-9 text-base">
-        Senior experience in front-end development, specializing in TypeScript,
-        React.js, and advanced patterns like hooks, Redux, and routing.
-        Dedicated to creating user-friendly, feature-rich websites. Proficient
-        in Mono repo tools, Styled components, Jest, Storybook, and Git.
+        Senior with 5+ years experience in front-end development, specializing
+        in TypeScript, React.js, and advanced patterns like hooks, Redux, and
+        routing. Dedicated to creating user-friendly, feature-rich websites.
+        Proficient in Mono repo tools, Styled components, Jest, Storybook, and
+        Git.
       </p>
 
       <div
@@ -86,24 +86,11 @@
           <FigmaIcon class="w-12 md:w-16 hover:text-neutral-content" />
         </div>
       </div>
-      <!-- <div class="flex gap-4 text-primary mb-9">
-        <SveleIcon class="" />
-        <ReactIcon class="" />
-        <ReduxIcon class="" />
-        <StorybookIcon class="" />
-        <TypescriptIcon class="" />
-        <JavascriptIcon class="" />
-        <CssIcon class="" />
-        <HtmlIcon class="" />
-        <JestIcon class="" />
-        <GitIcon class="" />
-        <FirebaseIcon class="" />
-        <FigmaIcon class="" />
-      </div> -->
+
       <div class=" join-vertical join md:join-horizontal gap-2">
         <a
           class="btn btn-lg md:btn-md btn-neutral btn-wide normal-case ring-primary"
-          href="/asset/Benjamin CV Fullstack Typescript - 2023.pdf"
+          href="/asset/Benjamin CV Frontend Typescript - 2024.pdf"
           target="_blank">Read my cv</a
         >
         <a
@@ -114,99 +101,7 @@
       </div>
     </div>
     <div class="h-24 lg:hidden" />
-    <!-- Chatt window -->
-    <!-- <div class="mockup-window border border-base-300">
-      <div
-        class="flex justify-center px-4 py-8 md:py-16 border-t border-base-300"
-      >
-        <div>
-          <div class="chat pt-4 md:pt-1 chat-start">
-            <div class="chat-image avatar">
-              <div class="w-10 rounded-full">
-                <img
-                  src="https://images.unsplash.com/photo-1535207010348-71e47296838a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODR8fGZhY2UlMjB3b21hbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
-                />
-              </div>
-            </div>
-            <div class="chat-header">
-              You
-              <time class="text-xs opacity-50">5 min ago</time>
-            </div>
-            <div class="chat-bubble">👋Hi Benji! Impressed by your skills!</div>
-          </div>
-          <div class="chat pt-4 md:pt-1 chat-end">
-            <div class="chat-image avatar">
-              <div class="w-10 rounded-full">
-                <img
-                  src="/asset/cv-no-smile-fancy-500.jpg"
-                />
-              </div>
-            </div>
-            <div class="chat-header">
-              Benji
-              <time class="text-xs opacity-50">3 min ago</time>
-            </div>
 
-            <div class="chat-bubble">😁Thanks! What's up?</div>
-          </div>
-
-          <div class="chat pt-4 md:pt-1 chat-start">
-            <div class="chat-image avatar">
-              <div class="w-10 rounded-full">
-                <img
-                  src="https://images.unsplash.com/photo-1535207010348-71e47296838a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODR8fGZhY2UlMjB3b21hbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
-                />
-              </div>
-            </div>
-            <div class="chat-header">
-              You
-              <time class="text-xs opacity-50">1 min ago</time>
-            </div>
-            <div class="chat-bubble">
-              📞 Interested in a chat? Drop your email. 😁😉
-            </div>
-          </div>
-          <div class="chat pt-4 md:pt-1 chat-end">
-            <div class="chat-image avatar">
-              <div class="w-10 rounded-full" />
-            </div>
-            <div class="chat-header">
-              Benji
-              <time class="text-xs opacity-50">Just now</time>
-            </div>
-            <div class="chat-bubble">
-              Sure 👍, here it is
-
-              <a
-                class="link link-primary"
-                href="hi@benjaminkarlsson.com"
-                target="_blank"
-                rel="noopener noreferrer">hi@benjaminkarlsson.com</a
-              >
-              or reach me at
-              <a
-                class="link link-primary"
-                href="https://www.linkedin.com/in/benjik/"
-                target="_blank"
-                rel="noopener noreferrer">linkedin.com/in/benjik</a
-              >
-            </div>
-          </div>
-          <div class="chat pt-4 md:pt-1 chat-end">
-            <div class="chat-image avatar">
-              <div class="w-10 rounded-full">
-                <img
-                  src="/asset/cv-no-smile-fancy-500.jpg"
-                />
-              </div>
-            </div>
-            <div class="chat-header" />
-            <div class="chat-bubble">Have a good time! 😉😁</div>
-          </div>
-        </div>
-      </div>
-    </div> -->
-    <!-- chat wwindow 2 -->
     <div class="mockup-window border border-base-300 backdrop-blur">
       <div
         class="flex justify-center px-4 py-8 md:py-16 border-t border-base-300"
--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
     import * as config from "$lib/config";
-    import { Facebook, Github, Instagram, Linkedin } from "lucide-svelte";
+    import { Github, Instagram, Linkedin } from "lucide-svelte";
 </script>
 
 <footer class="block px-6 py-24 sm:py-32 bg-base-200 justify-center">
     <div class="m-auto max-w-7xl">
         <h3 class="text-3xl font-bold tracking-tight sm:text-xl">
-            Is this the end? <img
+            Oh, this is a footer? <img
                 class="w-12 inline"
                 src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Face%20with%20Monocle.png"
                 alt="Star-Struck"
@@ -28,29 +28,8 @@
             <li class="">
                 <a class="btn btn-ghost inherit" href="/about">About</a>
             </li>
-            <!-- <li class="">
-                <a class="inherit decoration-0" href="/contact">Contact</a>
-            </li> -->
         </ul>
-        <!-- </div> -->
     </div>
-    <!-- Newsletter signup -->
-    <!-- <div class="flex justify-between pt-10 mt-20 border-t-base-300 border-t-2">
-        <div class="">
-            <h5 class="font-bold">Subscribe to our Newsletter</h5>
-            <p class="mt-2">
-                Latest news, articles and resources, sent to your inbox.
-            </p>
-        </div>
-        <div class="flex">
-            <input
-                type="text"
-                placeholder="Enter your email"
-                class="input input-bordered w-full max-w-xs"
-            /><button class="btn btn-primary normal-case ml-5">Subscribe</button
-            >
-        </div>
-    </div> -->
 
     <div
         class="max-w-7xl m-auto flex justify-between pt-10 mt-10 border-t-base-300 border-t-1 text-neutral-600"
